69c8b138f5ec02e175ab80aa97c582210e37a569
andrija.dragojevic <andrija@coinis.com>
2023-07-26 16:28:14 +0200

    Created a Team page with the table

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Header from '../../components/Header'
+import { Box } from '@mui/material'
 
 function index() {
   return (
-    <div>
-      
-    </div>
+    <Box m="20px">
+    <Box display="flex" justifyContent="space-between" alignItems="center">
+      <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
   )
 }
 
-export default index
+export default index;
